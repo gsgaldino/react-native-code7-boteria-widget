a0a70c03d43d34b6b8891389284a07102963cdd8
Gabriel Soares <gabrielsoaresii@outlook.com>
2023-01-04 08:36:30 -0300

    chore: change socket url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,4 @@
-export const SOCKET_URL =
-  'http://e626-2804-431-cff6-8636-88ae-3d03-ffcc-5e2d.ngrok.io';
-export const API_URL =
-  'http://3da8-2804-431-cff6-8636-88ae-3d03-ffcc-5e2d.ngrok.io';
+export const SOCKET_URL = 'http://eb8a-177-138-26-33.ngrok.io';
+export const API_URL = 'http://ce99-177-138-26-33.ngrok.io';
 export const GET_BOT_URL =
   'https://8g82aisqs3.execute-api.sa-east-1.amazonaws.com/test/getBotCdn/';
