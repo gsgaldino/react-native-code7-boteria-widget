17ab3adaf4801155f4bed33e15ab52f67d72a52d
Gabriel Soares <gabrielsoaresii@outlook.com>
2023-01-23 07:37:40 -0300

    chore: change env urls

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 export const SOCKET_URL =
-  'http://d68a-2804-431-cff6-5a7f-4c2b-32ad-e6ad-55f8.ngrok.io';
+  'http://0987-2804-431-cff6-5a7f-ed1e-e99f-875b-9063.ngrok.io';
 export const API_URL =
-  'http://9772-2804-431-cff6-5a7f-4c2b-32ad-e6ad-55f8.ngrok.io';
+  'http://7c77-2804-431-cff6-5a7f-ed1e-e99f-875b-9063.ngrok.io';
 export const GET_BOT_URL =
   'https://8g82aisqs3.execute-api.sa-east-1.amazonaws.com/test/getBotCdn/';
